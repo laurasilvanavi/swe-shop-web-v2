b615e4c684e931302f1a22c84d04cf38992a725e
Julius Burbulis <burbulis.julius@gmail.com>
2017-02-15 14:53:10 +0200

    [Dev] Added category module w/ component and routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'category',
+    component: CategoryComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class AppRoutingModule {
+}
